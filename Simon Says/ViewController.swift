86e949a0fc870dc8af70a78671ec2574cbf865f4
Kevin Vu <kevinvu@Kevins-MacBook-Pro.local>
2018-12-25 15:45:55 -0500

    Initial Commit

---
+++ Simon Says/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simon Says
+//
+//  Created by Kevin Vu on 12/25/18.
+//  Copyright © 2018 Hung Vu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
